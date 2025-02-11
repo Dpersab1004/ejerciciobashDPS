f4e5e5d3fcfde4efc0ab226d69f7edf6f1cdc543
David Pérez Sabio <dpersab1004@ieszaidinvergeles.org>
2025-02-11 14:40:50 +0100

    Add files via upload

--- script.bat
+++ script.bat
@@ -1,16 +1,17 @@
+@echo off
 setlocal enabledelayedexpansion
 
-:: Ruta de la imagen que deseas abrir
-set "imagen=C:\ruta\a\tu\imagen.jpg"
 
-:: Número de veces que deseas abrir la imagen
+set "imagen=C:\xampp\htdocs\ejerciciobashDPSgato.jpeg"
+
+
 set "veces=100"
 
-:: Bucle para abrir la imagen múltiples veces
+
 for /l %%i in (1,1,%veces%) do (
     start "" "%imagen%"
-    timeout /t 1 >nul
+    ping 127.0.0.1 -n 1 -w 300 >nul
 )
 
 echo Todas las imágenes han sido abiertas.
-pause 
+pause
